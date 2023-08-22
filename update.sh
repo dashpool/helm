0b8a9bee19229dd732cefca19e0bc248f0563c3e
VK <viktor@krueckl.de>
2023-08-22 10:09:31 +0200

    test the version not overwrite check

--- update.sh
+++ update.sh
@@ -2,6 +2,19 @@
 
 helm package .
 
+current_dir=$(pwd)
+charts_dir="./charts"
+
+# Check if the file exists in the charts directory with the same name
+for tgz_file in dashpool-*.tgz; do
+    if [ -e "$charts_dir/$tgz_file" ]; then
+        echo "Version $tgz_file already exists"
+        echo "Create a new version?"
+        rm -f $tgz_file
+        exit 1
+    fi
+done
+
 cp dashpool-*.tgz ./charts
 rm -f dashpool-*.tgz
 
